04e7dab2c6140fb204f011ce3994945c8cbe0608
Melvin7878 <90244833+Melvin7878@users.noreply.github.com>
2022-11-13 21:02:24 +0100

    Improvements and some progress towards trouble shooting
    Have found what prevents me from seeing the bullets.
    
    Have improved the structure on which I spawn the bullets
    
    Some problems to be fixed without the things mentioned before I can go on to work on the actual progress of the task.

--- Assets/Scripts/Tasks/LightningTask_Scripts/LightningGamePanel.cs
+++ Assets/Scripts/Tasks/LightningTask_Scripts/LightningGamePanel.cs
@@ -6,9 +6,9 @@ public class LightningGamePanel : MonoBehaviour
 {
     //Variables
     [Header("Create objects and reference")]
+
     GameObject lastFirstSelectedGameObject;
     [SerializeField] Task masterTask;
-
 
     [SerializeField] GameObject projectile;  //prefab bullet
     [SerializeField] List<GameObject> projectilesList = new List<GameObject>();
@@ -16,18 +16,16 @@ public class LightningGamePanel : MonoBehaviour
 
 
     [Header("Shooting/Position related variables")]
+
     [SerializeField] RectTransform shootingPosition1;
     [SerializeField] RectTransform shootingPosition2;
-    //private RectTransform[] shootingPositions = new RectTransform[2];
 
-    bool pos1Bullet = false;
-    bool pos2Bullet = false;
-
-    private int rotationValue = 60;
+    private int rotationValue = 57;
 
     //set a variable that handles the max amount of bullets before we're done with the task
 
     [Header("Resolve related variables")]
+
     [SerializeField] bool gameInitiated = false;
 
 
@@ -36,36 +34,19 @@ public class LightningGamePanel : MonoBehaviour
     {
         gameInitiated = true;
 
+        Debug.Log($"When game start - shoot position 1: {shootingPosition1.position} \n shoot position 2: {shootingPosition2.position}");
+
         if (gameInitiated)
         {
             InstantiateAndSetBullets(projectilesList, projectile, shootingPosition1, shootingPosition2);
         }
 
-        Debug.Log($"shoot position 1: {shootingPosition1} \n shoot position 2: {shootingPosition2}");
+        //Debug.Log($"shoot position 1: {shootingPosition1.position} \n shoot position 2: {shootingPosition2.position}");
         //SetValues(projectilesList, projectile, shootingPosition1, shootingPosition2);
     }
 
     private void Update()
     {
-        //Give the bullets a rigidbody
-        for (int i = 0; i < projectilesList.Count; i++)
-        {
-            if (i % 2 == 0)
-            {
-                pos2Bullet = true;
-                projectilesList[i].GetComponent<Rigidbody2D>().AddForce(new Vector2(-10, 10));
-                Debug.Log($"pos 2 bullets position: {projectilesList[i].transform.position}");
-                Debug.Log($"pos 2 bullet rotation: {projectilesList[i].transform.rotation}");
-            }
-            else
-            {
-                pos1Bullet = true;
-                projectilesList[i].GetComponent<Rigidbody2D>().AddForce(new Vector2(10, 10));
-                Debug.Log($"pos 1 bullets position: {projectilesList[i].transform.position}");
-                Debug.Log($"pos 1 bullet rotation: {projectilesList[i].transform.rotation}");
-            }
-        }
-
         ////if the task isn't completed yet
         //if (projectilesList.Count)
         //{
@@ -74,36 +55,52 @@ public class LightningGamePanel : MonoBehaviour
         //Debug.Log(projectileCounter);
     }
 
+    private void FixedUpdate()
+    {
+        //Give the bullets a rigidbody and add force
+        for (int i = 0; i < projectilesList.Count; i++)
+        {
+            if (i % 2 != 0)
+            {
+                projectilesList[i].GetComponent<Rigidbody2D>().AddForce(new Vector2(100, 100));
+                //Debug.Log($"pos 1 bullet's position: {projectilesList[i].transform.position}");
+                //Debug.Log($"pos 1 bullet rotation: {projectilesList[i].transform.rotation}");
+            }
+            else
+            {
+                projectilesList[i].GetComponent<Rigidbody2D>().AddForce(new Vector2(-100, 100));
+                //Debug.Log($"pos 2 bullets position: {projectilesList[i].transform.position}");
+                //Debug.Log($"pos 2 bullet rotation: {projectilesList[i].transform.rotation}");
+            }
+        }
+    }
+
     #region Shooting related functions
     void InstantiateAndSetBullets(List<GameObject> bulletsCollection, GameObject bullet, RectTransform shootPos1, RectTransform shootPos2)
     {
         Vector2 pos1Position = new Vector2(shootPos1.position.x, shootPos1.position.y);
         Vector2 pos2Position = new Vector2(shootPos2.position.x, shootPos2.position.y);
 
-        //Instantiate the "starting" couple bullets
+        //Instantiate the "starting" bullets
         for (int i = 0; i < projectileAmount; i++)
         {
             Instantiate(bullet);
 
             bulletsCollection.Add(bullet);
-            Debug.Log(bulletsCollection.Count);
         }
 
         //Set position of bullets
         for (int i = 0; i < bulletsCollection.Count; i++)
         {
-            Debug.Log("in for loop");
-            if (i % 2 != 2)          //bullet 1 position
+            if (i % 2 != 0)          //bullets at position 1
             {
                 bulletsCollection[i].transform.position = pos1Position;
-                Debug.Log(bulletsCollection[i].transform.position);
-                pos1Bullet = true;
+                Debug.Log($"bullet pos 1 start position: {bulletsCollection[i].transform.position}");
             }
-            else       //bullet 2 position
+            else     //bullets at position 2
             {
                 bulletsCollection[i].transform.position = pos2Position;
-                Debug.Log(bulletsCollection[i].transform.position);
-                pos2Bullet = true;
+                Debug.Log($"bullet pos 2 start position: {bulletsCollection[i].transform.position}");
             }
         }
 
@@ -112,17 +109,15 @@ public class LightningGamePanel : MonoBehaviour
         {
             bulletsCollection[i].transform.rotation = Quaternion.identity;
 
-            if (i % 2 != 2)     //bullets at position 1
+            if (i % 2 != 0)     //bullets at position 1
             {
-                bulletsCollection[i].transform.rotation = Quaternion.Euler(rotationValue, 0, 0);
-                Debug.Log($"pos 1 rotation: {bulletsCollection[i].transform.rotation.x}");
-                pos1Bullet = true;
+                bulletsCollection[i].transform.rotation = Quaternion.Euler(0, 0, rotationValue);
+                Debug.Log($"bullets pos 1 rotation: {bulletsCollection[i].transform.rotation.z}");
             }
-            else     //bullets at position 2 
+            else    //bullets at position 2 
             {
-                bulletsCollection[i].transform.rotation = Quaternion.Euler(-rotationValue, 0, 0);
-                Debug.Log($"pos 2 rotation: {bulletsCollection[i].transform.rotation.x}");
-                pos2Bullet = true;
+                bulletsCollection[i].transform.rotation = Quaternion.Euler(0, 0, -rotationValue);
+                Debug.Log($"bullets pos 2 rotation: {bulletsCollection[i].transform.rotation.z}");
             }
         }
     }
