7506dc57069b111af8ef6fa494b128c69e26eaf4
Melvin7878 <90244833+Melvin7878@users.noreply.github.com>
2022-11-20 21:58:46 +0100

    The velocity of the bullets is adjusted as well as set a progression score

--- Assets/Scripts/Tasks/LightningTask_Scripts/LightningGamePanel.cs
+++ Assets/Scripts/Tasks/LightningTask_Scripts/LightningGamePanel.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class LightningGamePanel : MonoBehaviour
 {
     [Header("Create objects and reference")]
 
+    //"General" variables
     GameObject lastFirstSelectedGameObject;
     [SerializeField] Task masterTask;
     [SerializeField] Transform canvasParent;
 
+    //Shooting related objects and references
     [SerializeField] GameObject preFabProjectile;  //prefab bullet
-    private GameObject bullet1;
-    private GameObject bullet2;
-
     [SerializeField] List<GameObject> projectilesList = new List<GameObject>();
     private int projectileAmount = 2;
 
+    //Completion related objects and references
+    [SerializeField] TextMeshProUGUI progressCounter;
 
     [Header("Shooting/Position related variables")]
 
@@ -26,6 +29,18 @@ public class LightningGamePanel : MonoBehaviour
     private int rotationValue = 60;
 
     //set a variable that handles the max amount of bullets before we're done with the task
+
+
+    [Header("Completion related variables")]
+
+    //Tesla detection related variables
+    CircleCollider2D teslaTrigger;
+
+
+    //Score related variables
+    private const int completionScore = 5;
+
+
 
     [Header("Resolve related variables")]
 
@@ -41,14 +56,28 @@ public class LightningGamePanel : MonoBehaviour
 
         if (gameInitiated)
         {
-            InstantiateAndSetBullets(projectilesList, preFabProjectile, bullet1, bullet2, shootingPosition1, shootingPosition2, canvasParent);
+            InstantiateAndSetBullets(projectilesList, preFabProjectile, shootingPosition1, shootingPosition2, canvasParent);
         }
+
+        //Set the beginning score
+        progressCounter.text = $"0 / {completionScore}";
     }
 
     private void Update()
     {
+        //Check if the bullet is inside the area of the tesla
+
+
+
+
+        //update progress
+
+
+
+
+
         ////if the task isn't completed yet
-        //if (projectilesList.Count)
+        //if ()
         //{
         //    projectileCounter = bulletsCollection.Count;
         //}
@@ -62,18 +91,17 @@ public class LightningGamePanel : MonoBehaviour
         {
             if (i % 2 != 0)     //bullet at position 1
             {
-                projectilesList[i].GetComponent<Rigidbody2D>().AddForce(new Vector2(2, 2));
+                projectilesList[i].GetComponent<Rigidbody2D>().AddForce(new Vector2(-2, 3));
             }
             else    //bullets at position 2
             {
-                projectilesList[i].GetComponent<Rigidbody2D>().AddForce(new Vector2(-2, 2));
+                projectilesList[i].GetComponent<Rigidbody2D>().AddForce(new Vector2(2, 3));
             }
         }
     }
 
     #region Shooting related functions
-    void InstantiateAndSetBullets(List<GameObject> bulletsCollection, GameObject bulletPreFab, GameObject bullet1, GameObject bullet2, 
-        RectTransform shootPos1, RectTransform shootPos2, Transform parent)
+    private void InstantiateAndSetBullets(List<GameObject> bulletsCollection, GameObject bulletPreFab, RectTransform shootPos1, RectTransform shootPos2, Transform parent)
     {
         Vector2 pos1Position = new Vector2(shootPos1.position.x, shootPos1.position.y);
         Vector2 pos2Position = new Vector2(shootPos2.position.x, shootPos2.position.y);
@@ -82,42 +110,12 @@ public class LightningGamePanel : MonoBehaviour
         //for (int i = 0; i < projectileAmount; i++)
         //{
         //Bullet 1 instantiated and added to the list
-        bullet1 = Instantiate(bulletPreFab, pos1Position, Quaternion.Euler(0, 0, rotationValue), parent);
+        GameObject bullet1 = Instantiate(bulletPreFab, pos1Position, Quaternion.Euler(0, 0, rotationValue), parent);
         bulletsCollection.Add(bullet1);
 
-        //Bullet 1 instantiated and added to the list
-        bullet2 = Instantiate(bulletPreFab, pos2Position, Quaternion.Euler(0, 0, -rotationValue), parent);
+        //Bullet 2 instantiated and added to the list
+        GameObject bullet2 = Instantiate(bulletPreFab, pos2Position, Quaternion.Euler(0, 0, -rotationValue), parent);
         bulletsCollection.Add(bullet2);
-        //}
-
-
-        ////Set position of bullets
-        ////bullets positioned at pos 2 (even numbers)
-        //for (int i = 0; i < bulletsCollection.Count; i += 2)
-        //{
-        //    bulletsCollection[i].transform.position = pos2Position;
-        //    Debug.Log($"bullet pos 2 start position: {bulletsCollection[i].transform.position}");
-        //}
-        ////bullets positioned at pos 1 (uneven numbers)
-        //for (int i = 1; i < bulletsCollection.Count; i += 2)
-        //{
-        //    bulletsCollection[i].transform.position = pos1Position;
-        //    Debug.Log($"bullet pos 1 start position: {bulletsCollection[i].transform.position}");
-        //}
-
-
-        //////Set the rotation of the bullets
-        ////bullets rotated at pos 2 (even numbers)
-        //for (int i = 0; i < bulletsCollection.Count; i += 2)
-        //{
-        //    bulletsCollection[i].transform.rotation = Quaternion.Euler(0, 0, -rotationValue);
-        //    Debug.Log($"bullet pos 2 start position: {bulletsCollection[i].transform.rotation.z}");
-        //}
-        ////bullets rotated at pos 1 (uneven numbers)
-        //for (int i = 1; i < bulletsCollection.Count; i += 2)
-        //{
-        //    bulletsCollection[i].transform.rotation = Quaternion.Euler(0, 0, rotationValue);
-        //    Debug.Log($"bullet pos 1 start position: {bulletsCollection[i].transform.rotation.z}");
         //}
     }
     #endregion
